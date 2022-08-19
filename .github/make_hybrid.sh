3126c6b5599d5b555addf1d4193aeb8a62483a57
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-19 15:53:54 +0200

    fix: more xo ignores

--- .github/make_hybrid.sh
+++ .github/make_hybrid.sh
@@ -30,7 +30,7 @@ PJSON=$(cat package.json | jq --tab '
 	| .typesVersions["*"]["cjs/index.d.ts"] = ["esm/index.d.ts"]
 	| .typesVersions["*"]["*"] = ["esm/*"]
 	| .scripts["to-cjs"] = "esm2cjs --in esm --out cjs -t node12"
-	| .xo = {ignores: ["cjs"]}
+	| .xo = {ignores: ["cjs", "**/*.test-d.ts", "**/*.d.ts"]}
 ')
 echo "$PJSON" > package.json
 
